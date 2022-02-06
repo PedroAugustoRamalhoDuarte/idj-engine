a837830369099867fdc60e953fd9ff941cd744b3
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2022-02-06 11:48:32 -0300

    Initialize Vec2 and Rect classes

---
+++ include/Rect.h
@@ -0,0 +1,13 @@
+//
+// Created by pedro on 06/02/2022.
+//
+
+#ifndef IDJ_ENGINE_RECT_H
+#define IDJ_ENGINE_RECT_H
+
+class Rect {
+private:
+    float x, y, w, h;
+};
+
+#endif //IDJ_ENGINE_RECT_H
---
+++ include/Vec2.h
@@ -0,0 +1,14 @@
+//
+// Created by pedro on 06/02/2022.
+//
+
+#ifndef IDJ_ENGINE_VEC2_H
+#define IDJ_ENGINE_VEC2_H
+
+class Vec2 {
+private:
+    float x, y;
+};
+
+
+#endif //IDJ_ENGINE_VEC2_H
